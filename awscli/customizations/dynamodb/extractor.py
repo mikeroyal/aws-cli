8cc0f7f919ae1a555907e3f657357e69183f5fee
JordonPhillips <phjordon@amazon.com>
2018-11-18 22:14:16 -0800

    replacement substitutor

--- awscli/customizations/dynamodb/extractor.py
+++ awscli/customizations/dynamodb/extractor.py
@@ -10,38 +10,103 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from .lexer import Lexer
+from .parser import Parser
 
 
 class AttributeExtractor(object):
-    def __init__(self, lexer=None):
-        self._lexer = lexer
-        if lexer is None:
-            self._lexer = Lexer()
+    COMPARATORS = {
+        'eq': '=',
+        'ne': '<>',
+        'lt': '<',
+        'lte': '<=',
+        'gt': '>',
+        'gte': '>=',
+    }
 
-    def extract(self, expression, index_start=0):
-        tokens = list(self._lexer.tokenize(expression))
-        identifiers = {}
-        values = {}
-        new_expression = ''
-        index = index_start
-        for token in tokens:
-            if token['type'] in ['unquoted_identifier', 'identifier']:
-                substitution_token = '#n%s' % index
-                identifiers[substitution_token] = token['value']
-                new_expression += substitution_token
-                index += 1
-            elif token['type'] == 'literal':
-                substitution_token = ':n%s' % index
-                values[substitution_token] = token['value']
-                new_expression += substitution_token
-                index += 1
-            else:
-                new_expression += token['value']
+    def __init__(self, parser=None):
+        self._parser = parser
+        if parser is None:
+            self._parser = Parser()
+        self._identifiers = {}
+        self._literals = {}
+        self._index_offset = 0
 
+    def extract(self, expression, index_offset=0):
+        self._identifiers = {}
+        self._literals = {}
+        self._index_offset = index_offset
+        root = self._parser.parse(expression)
+        expression = self._visit(root).strip()
         return {
-            'expression': new_expression,
-            'identifiers': identifiers,
-            'values': values,
-            'substitution_count': index - index_start,
+            'expression': expression,
+            'identifiers': self._identifiers,
+            'values': self._literals,
         }
+
+    def _substitution_index(self):
+        num_substituted = len(self._identifiers) + len(self._literals)
+        return num_substituted + self._index_offset
+
+    def _visit(self, node):
+        method = getattr(self, '_visit_%s' % node['type'])
+        return method(node)
+
+    def _default_visit(self, node):
+        raise NotImplementedError('_default_visit')
+
+    def _visit_comparator(self, node):
+        left = self._visit(node['children'][0])
+        right = self._visit(node['children'][1])
+        expression = '%s%s %s' % (
+            left, self.COMPARATORS[node['value']], right
+        )
+        return expression
+
+    def _visit_identifier(self, node):
+        identifier_replacement = '#n%s' % self._substitution_index()
+        self._identifiers[identifier_replacement] = node['value']
+        return '%s ' % identifier_replacement
+
+    def _visit_literal(self, node):
+        literal_replacement = ':n%s' % self._substitution_index()
+        self._literals[literal_replacement] = node['value']
+        return '%s ' % literal_replacement
+
+    def _visit_sequence(self, node):
+        expression = '('
+        for child in node['children']:
+            expression += self._visit(child)
+        expression += ') '
+        return expression
+
+    def _visit_or_expression(self, node):
+        left = self._visit(node['children'][0])
+        right = self._visit(node['children'][1])
+        return '%sOR %s' % (left, right)
+
+    def _visit_and_expression(self, node):
+        left = self._visit(node['children'][0])
+        right = self._visit(node['children'][1])
+        return '%sAND %s' % (left, right)
+
+    def _visit_not_expression(self, node):
+        return 'NOT %s' % self._visit(node['children'][0])
+
+    def _visit_subexpression(self, node):
+        return '( %s) ' % self._visit(node['children'][0])
+
+    def _visit_function(self, node):
+        return '%s%s' % (node['value'], self._visit_sequence(node))
+
+    def _visit_in_expression(self, node):
+        return '%sIN %s' % (
+            self._visit(node['children'][0]),
+            self._visit(node['children'][1]),
+        )
+
+    def _visit_between_expression(self, node):
+        return '%sBETWEEN %sAND %s' % (
+            self._visit(node['children'][0]),
+            self._visit(node['children'][1]),
+            self._visit(node['children'][2]),
+        )
